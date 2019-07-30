342ef31d2fe02c519d3428ac7e8d3242b9c81b7c
erdem tuna <erdemtuna@yandex.com>
2019-07-30 16:27:21 +0000

    Successful run

---
+++ dict.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from pymongo import MongoClient
+import json
+
+client = MongoClient('<Connection String Obtained from your MongoDB account')
+db = client.get_database('<Your Database Name')
+#collection name inside your database
+trans = db.translations
+
+#print(trans.count_documents({}))
+
+dict_keys = ('word', 'means', 'use')
+states = ('1', 'word', '2', 'quit')
+more_words = ('y', 'yes', 'n', 'no')
+
+QUIT = False
+
+while not (QUIT):
+    #determine the state
+    user_in = input('Word(1) or Quit(2): ')
+    #turn input to lowercase
+    user_low = user_in.lower()
+    #array of json objects
+    temp_array = []
+    #user wants to enter new word
+    if(user_low == states[0] or user_low == states[1]):
+        #want to continue adding words?
+        more = True
+        while(more):
+            #the temporary json object to store user input
+            temp_json = {}
+            for  key in dict_keys:
+                if(key == dict_keys[0]):
+                    #get the original word
+                    word_in = input('Enter original word: ')
+                    #dump to json in all lowercase
+                    temp_json[key] = word_in.lower()
+                elif(key == dict_keys[1]):
+                    #get the translation
+                    means_in = input('Enter the translation: ')
+                    #dump to json in all lowercase
+                    temp_json[key] = means_in.lower()
+                elif(key == dict_keys[2]):
+                    #get the sample sentence
+                    use_in = str(input('Enter the sample sentence: '))
+                    #dump to json
+                    temp_json[key] = use_in
+                    command_in = input('Want to add more words? (Y)es or (N)o?')
+                    command_low = command_in.lower()
+                    #user doesn't want to add more words
+                    if(command_low != more_words[0] and command_low != more_words[1]):
+                        more = False
+                    #dump json object to json array
+                    temp_array.append(temp_json)
+        #convert array to json array
+        json_array = json.loads(json.dumps(temp_array, ensure_ascii = False).encode('utf8'))
+        #insert to database
+        print(json_array)
+        trans.insert_many(json_array)  
+    #user wants to quit
+    elif(user_low == states[2] or user_low == states[3]):
+        QUIT = True
+        print('Successful.')
+    #not a valid command
+    else:
+        print('Not a valid command')
